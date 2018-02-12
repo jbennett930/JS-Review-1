4822102d1ed9b7087827ed1672ef5c6855f2a760
Unknown <jbennett930@west-mec.org>
2018-02-12 00:12:25 -0700

    Added files to start the function
    Added some of the files to make it a repository . In the files i created a function to help display the payroll of employees.

---
+++ er.js
@@ -0,0 +1,15 @@
+function calcPay() {
+  var employees = ['Bob', 'Billy', 'Bente',  'Woody', 'Jacson', 'Michael', 'Heather', 'Jennifer', 'John', 'David'];
+  var hoursWorked = ['25', '30', '25',  '20', '15', '17', '40', '35', '27', '14'];
+  var hourlyRate = ['10', '11', '14',  '12', '15', '11', '10', '12', '14', '16'];
+  var total = "";
+
+for (var i = 0; i < 11; i++) {
+  if  {
+
+  }
+}
+}
+
+document.getElementById('total').innerHTML = employees[i] + hoursWorked[i] + hourlyRate[i];
+document.write(total);
